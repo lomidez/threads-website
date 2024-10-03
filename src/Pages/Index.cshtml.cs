d9ad1d5941f16e3079d94d5764b53dda32901b84
Jooa Lee <jlee54@seattleu.edu>
2024-10-02 18:25:19 -0700

    Added name Jooa Lee to IndexModel Comment in index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary> 
-    /// Lisa Lomidze, Isaac Yushaiyin, Jacobie Fullerton, Jooa Lee
+    /// Lisa Lomidze, Isaac Yushaiyin, Jooa Lee, Jacobie Fullerton
     /// </summary> 
     public class IndexModel : PageModel
     {
