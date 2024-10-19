da30e4221878f1c6c4e3647c8d7dd19ffb224b38
Isaac Abudukeyoumu <yabudukeyoumu@seattleu.edu>
2024-10-18 21:22:40 -0700

    "Added TagModel.cs for tagging functionality"

---
+++ src/Models/TagModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ContosoCrafts.WebSite.Models
+{
+    public class TagModel
+    {
+        public int TagID { get; set; } // unique ID for each tag
+        public string TagName { get; set; } // Name of the tag (e.g., "shoes", " t-shirt" , "jacket")
+    }
+}
