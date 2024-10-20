0f7b947b5829c03dfd74734f4949946ba2154c6d
Jooa Lee <jlee54@seattleu.edu>
2024-10-20 03:54:27 -0700

    created TagService.cs file rough draft in the Service folder to handle retrieving tags by category would go

---
+++ src/Services/TagServices.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+
+//public class TagService
+//{
+	// db context
+	// private readonly string_jsonFilePath =
+	// private readonly YourDbContext _context;	
+	
+
+    // if project uses as the data source instead of SQl 
+    // json
+    // ["TagId": 1, "TagName": "bag", "TagCategory": "Type"]
+
+	//public Dictionary<String, List<TagModel>> GetTagsByCategory()
+	//{
+		// read and parse json file 
+		// get gas 
+
+		// group the tags by category
+		//return grouped tags;
+	//}
+//}
