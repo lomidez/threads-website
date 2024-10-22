2ea1f36a9e0fa73bedd3f78788447bce99380292
Jooa Lee <jlee54@seattleu.edu>
2024-10-22 00:44:52 -0700

    added Tag values for tag category in the TagModel.cs file. for example, if the category is the size, we have small, medium, large values of it.

--- src/Models/TagModel.cs
+++ src/Models/TagModel.cs
@@ -1,9 +1,11 @@
-﻿namespace ContosoCrafts.WebSite.Models
+﻿using System.Collections.Generic;
+namespace ContosoCrafts.WebSite.Models
 {
     public class TagModel
     {
         public int TagID { get; set; } // unique ID for each tag
         public string TagName { get; set; } // Name of the tag (e.g., "shoes", " t-shirt" , "jacket")
         public string TagCategory { get; set; } //Category: Type, size, Color, Material, Style etc.
+        public List<string> TagValues { get; set; } //i.e size has small, medium, large
     }
 }
