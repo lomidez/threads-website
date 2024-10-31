4131c7b0116e515fb49e810614cecac3a064babb
Isaac Abudukeyoumu <yabudukeyoumu@seattleu.edu>
2024-10-31 15:02:23 -0700

    Update to handle nullable Quantity, Ratings, and add Weight and Dimensions properties in ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -52,12 +52,10 @@
 //    }
 //}
 
-
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
-using ContosoCrafts.WebSite.Models;
 
 namespace ContosoCrafts.WebSite.Models
 {
@@ -76,31 +74,44 @@ namespace ContosoCrafts.WebSite.Models
 
         public string Description { get; set; }
 
-        // Uncommented Ratings Property
+        // Ratings should match JSON data type (if null, make it nullable int array)
         public int[] Ratings { get; set; } = new int[] { };
 
         public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
 
-        public string Quantity { get; set; }
+        // Update Quantity to nullable int if the JSON values are numbers
+        public int? Quantity { get; set; }
 
-        [Range(-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [Range(0, int.MaxValue, ErrorMessage = "Value for {0} must be non-negative.")]
         public int Price { get; set; }
 
         // Store the Comments entered by the users on this product
         public List<CommentModel> CommentList { get; set; } = new List<CommentModel>();
 
-        // Store the tags associated with this product (fixed the typo)
+        // Store tags associated with this product
         public List<TagModel> Tags { get; set; } = new List<TagModel>();
 
-        // Store all tags for Category, Size, Color, Material, and Style
+        // Additional attributes
         public string Category { get; set; }
         public string Size { get; set; }
         public string Color { get; set; }
         public List<string> Material { get; set; } = new List<string>();
         public List<string> Style { get; set; } = new List<string>();
 
-        // ToString method to serialize the ProductModel (only one instance now)
-        public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
+        // Additional fields for Weight and Dimensions
+        public decimal Weight { get; set; }
+
+        public Dimensions Dimentions { get; set; } = new Dimensions();
+
+        // ToString method to serialize the ProductModel
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+
+    public class Dimensions
+    {
+        public decimal Length { get; set; }
+        public decimal Width { get; set; }
+        public decimal Height { get; set; }
     }
 }
 
