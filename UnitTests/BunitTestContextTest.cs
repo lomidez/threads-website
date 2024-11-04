fb0d5e06f0fd0fe19f8ac018b4811caed40b8c63
Isaac Abudukeyoumu <yabudukeyoumu@seattleu.edu>
2024-11-03 21:16:36 -0800

    Add unit tests for Bunit test context setup and teardown functionality in BunitTestContextTests.cs

---
+++ UnitTests/BunitTestContextTest.cs
@@ -0,0 +1,34 @@
+﻿using Bunit;
+using NUnit.Framework;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace UnitTests
+{
+    public class BunitTestContextTests : BunitTestContext
+    {
+        [Test]
+        public void TestContext_ShouldInitialize_AndDisposeCorrectly()
+        {
+            // Arrange
+            var dummyComponent = TestContext.RenderComponent<DummyComponent>();
+
+            // Act
+            dummyComponent.MarkupMatches("<p>Hello, Bunit!</p>");
+
+            // Assert
+            Assert.That(TestContext, Is.Not.Null);
+        }
+
+        public class DummyComponent : ComponentBase
+        {
+            protected override void BuildRenderTree(RenderTreeBuilder builder)
+            {
+                builder.OpenElement(0, "p");
+                builder.AddContent(1, "Hello, Bunit!");
+                builder.CloseElement();
+            }
+        }
+    }
+}
+
