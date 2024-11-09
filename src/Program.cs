822162ebf3d97e97fed486867053cae1d00fa9a2
Jooa Lee <jlee54@seattleu.edu>
2024-11-09 13:41:18 -0800

    added comments in Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -3,17 +3,22 @@ using Microsoft.Extensions.Hosting;
 
 namespace ContosoCrafts.WebSite
 {
+    // Entry point for the application
     public class Program
     {
+        // Main method, which is the entry point of the application
         public static void Main(string[] args)
         {
+            // Builds and runs the application host
             CreateHostBuilder(args).Build().Run();
         }
 
+        // Creates and configures the web host builder
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            Host.CreateDefaultBuilder(args) // Create a default host builder with configuration
+                .ConfigureWebHostDefaults(webBuilder => // Configure web host defaults
                 {
+                    // Specifies the Startup class to configure services and middleware
                     webBuilder.UseStartup<Startup>();
                 });
     }
