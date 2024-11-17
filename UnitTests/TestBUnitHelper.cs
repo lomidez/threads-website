68e2aef8ba652a7762122e9375d620f5e2f2e0c9
Jooa Lee <jlee54@seattleu.edu>
2024-11-16 23:24:25 -0800

    added XML comments in TestBUnitHelper.cs

--- UnitTests/TestBUnitHelper.cs
+++ UnitTests/TestBUnitHelper.cs
@@ -4,15 +4,23 @@ using NUnit.Framework;
 namespace UnitTests
 {
     /// <summary>
-    /// Test Context used by bUnit
+    /// Provides a test context used by bUnit for component testing.
+    /// This abstract class sets up and tears down the bUnit <see cref="TestContext"/>
+    /// for each test to ensure proper initialization and resource cleanup.
     /// </summary>
     public abstract class BunitTestContext : TestContextWrapper
     {
-        // The Setup sets the context
+        /// <summary>
+        /// Sets up the <see cref="TestContext"/> before each test.
+        /// This method is executed before each test to initialize the bUnit context.
+        /// </summary>
         [SetUp]
         public void Setup() => TestContext = new Bunit.TestContext();
 
-        // When done displose removes it, to free up system resources
+        /// <summary>
+        /// Tears down the <see cref="TestContext"/> after each test.
+        /// This method is executed after each test to dispose of the context and free up system resources.
+        /// </summary>
         [TearDown]
         public void TearDown() => TestContext.Dispose();
     }
