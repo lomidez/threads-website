c53e7f80e380a9e0f5cba30f36d89fdb4c1e2059
Jooa Lee <jlee54@seattleu.edu>
2024-11-28 16:06:48 -0800

    added Enum for product colors in ProductColor.cs

---
+++ src/Models/Enums/ProductColor.cs
@@ -0,0 +1,20 @@
+namespace ContosoCrafts.WebSite.Models
+{
+    public enum ProductColor
+    {
+        red, 
+        green,
+        blue,
+        purple,
+        yellow,
+        brown, 
+        pink, 
+        orange,
+        white, 
+        black, 
+
+    }
+}
+
+
+
