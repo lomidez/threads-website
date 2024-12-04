e23d967c18291a5d837883751905b6f556bd1b1a
Isaac Abudukeyoumu <yabudukeyoumu@seattleu.edu>
2024-12-03 17:16:26 -0800

    Created ProductCategory.cs

---
+++ src/Models/Enums/ProductCategory.cs
@@ -0,0 +1,16 @@
+﻿namespace ContosoCrafts.WebSite.Models
+{
+    public enum ProductCategory
+    {
+        bag,
+        jacket,
+        Trousers,
+        TShirt,
+        Electronics,
+        Clothing,
+        Category1,
+        Category2,
+
+    }
+}
+
